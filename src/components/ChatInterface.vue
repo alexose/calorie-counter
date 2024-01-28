53ded9e6d6e8d1fa0ff5bc937c9934fce7b54ead
Alexander Ose <alexander@parsely.com>
2024-01-28 10:31:44 -0800

    Add chat interface scaffold

---
+++ src/components/ChatInterface.vue
@@ -0,0 +1,18 @@
+<script>
+    export default {
+        name: "ChatInterface",
+        data() {
+            return {};
+        },
+        methods: {},
+        mounted() {},
+    };
+</script>
+
+<template>
+    <div>
+        <h1>Chat Interface</h1>
+    </div>
+</template>
+
+<style scoped></style>
